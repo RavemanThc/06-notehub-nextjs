8b3990bec53397a1e3ac602821462889b51bfe3f
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-04-19 22:28:28 +0300

    2

---
+++ app/notes/[id]/error.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+type Props = {
+    error: Error;
+    reset: () => void;
+};
+
+const Error = ({error, reset}: Props) => {
+    return(
+        <div>
+            <p>Could not fetch the list of notes. {error.message}</p>
+            <p>{error.message}</p>
+            <button onClick={reset}> Спробувати знову</button>
+        </div>
+    );
+};
+
+export default Error;
